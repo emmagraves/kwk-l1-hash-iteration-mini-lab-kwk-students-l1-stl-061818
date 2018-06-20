c2f2bf48bbaadb971e02acb4753d3e1e895c49c3
emmagraves <emma.graves09@gmail.com>
2018-06-20 19:30:14 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -17,7 +17,7 @@ def add_a_key_value_pair
     "beijing" => "2008", 
     "london" => "2012"
   }
-  summer_olympics["atlanta"] = "1996"
+  #summer_olympics["atlanta"] = "1996"
 end
 
 puts add_a_key_value_pair
