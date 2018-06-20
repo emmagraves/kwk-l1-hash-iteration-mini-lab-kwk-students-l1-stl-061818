c00d054bbdaba3a975ffe3f46e2b38f9fb693883
emmagraves <emma.graves09@gmail.com>
2018-06-20 19:18:27 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -8,7 +8,8 @@ end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-summer_olympics.push["atlanta",]
+  summer_olympics["atlanta"] = "1996"
+  puts summer_olympics
 end
 
 def iterate_through_hash
@@ -20,3 +21,5 @@ def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 end
+
+add_a_key_value_pair
